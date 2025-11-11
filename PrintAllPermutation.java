03b01ddda1f486b6db486f0b0e651cf884789bd3
Anshika Yadav <anshiy314@gmail.com>
2025-11-11 11:55:43 +0530

    Add files via upload

---
+++ PrintAllPermutation.java
@@ -0,0 +1,29 @@
+public class PrintAllPermutation{
+    public static void permute(int[] nums){
+        boolean[] used =new boolean[nums.length];
+        List<Integer> path= new ArrayList<>();
+        backtrack(nums, used,path);   
+    }
+    private static void backtrack(int[] nums, boolean[] used, List<Integer>path){
+        //  base case 
+        if(path.size()==nums.length){
+            System.out.println(path);
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            used[i]=true;
+            path.add(nums[i]);
+            backtrack(nums,used,path);
+
+            // undo backtrack 
+            path.remove(path.size()-1);
+            used[i]=false;
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr={1,2,3};
+        permute(arr);
+    }
+}
